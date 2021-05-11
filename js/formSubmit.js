f0ead3b5345467d3d178995b1f75cbe5b43cc9f0
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-05-11 11:19:38 +0300

    form

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -4,4 +4,8 @@ formDOM.addEventListener('submit', formSubmit);
 function formSubmit() {
   event.preventDefault(); // default islemi engelledik...
   console.log('islem gerceklestirildi');
+
+  let scoreIntputDOM = document.querySelector('#score');
+  console.log(scoreIntputDOM.value);
+  localStorage.setItem('score', scoreIntputDOM.value);
 }
