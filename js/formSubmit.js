b60b19dde327bb4a7ce3c9d659d143f63367cebe
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-05-11 10:14:27 +0300

    default islemi engelleme

---
+++ js/formSubmit.js
@@ -0,0 +1,7 @@
+let formDOM = document.querySelector('#userForm');
+formDOM.addEventListener('submit', formSubmit);
+
+function formSubmit() {
+  event.preventDefault(); // default islemi engelledik...
+  console.log('islem gerceklestirildi');
+}
